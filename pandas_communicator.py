32c40f8d09f12423f0da6afc0f5b6b5f966d27c3
charleseMuchmore <charlymuchmore@gmail.com>
2022-09-29 20:15:30 -0700

    improved UI

--- pandas_communicator.py
+++ pandas_communicator.py
@@ -12,7 +12,7 @@ class PandasCommunicator:
         return pandas.to_list(csv_filename)
 
     def loc_dataframe(dataframe, list_of_column_names):
-        return dataframe.loc[:, list_of_column_names]
+        return dataframe.loc[list_of_column_names]
 
     def iterate(dataframe):
         return dataframe.__iter__()
@@ -23,5 +23,19 @@ class PandasCommunicator:
     def columns(dataframe):
         return dataframe.columns
 
-    def DataFrame(data_to_frame):
-        return pandas.DataFrame(data_to_frame)
+    def DataFrame(data, columns=None, dtype=None):
+        return pandas.DataFrame(data, columns, dtype)
+
+    def series_to_list(series):
+        return pandas.Series.tolist(series)
+
+    def Series(data):
+        return pandas.Series(data)
+
+    def rename(dataframe, mapper, axis):
+        """mapper is dict that looks like this: {old_name1 : new_name1, old_name2 : new_name2}. Axis is either 0 or 1: 0=y, 1=x"""
+        return dataframe.rename(mapper, axis=axis)
+
+    def hide_index(dataframe):
+        return dataframe.style.hide_index(dataframe)
+    
