29294a1fc7aebe2266ed82890de0b6b4d4daa499
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2022-10-01 21:03:29 -0700

    User interface (#5)
    * started pandas communication
    
    * fully switched to pandas
    
    * improved UI
    
    * deleted method
    
    * added outline readme for commands
    
    * added title

--- __main__.py
+++ __main__.py
@@ -2,16 +2,14 @@ from pandas_communicator import PandasCommunicator
 from budget_calculator import BudgetCalculator
 from dict_iterator import DictIterator
 from user_display import UserDisplay
+from title import Title
 
+title = Title()
 
-user_input_dollars_to_budget = int(input("Income Amount: $"))
-
+user_input_dollars_to_budget = int(title.print_title())
+# user_input_dollars_to_budget = int(input("Income Amount: $"))
 dataframe = PandasCommunicator.read_csv("regular_budget_plan.csv") #loads dataframe from csv
 dataframe = BudgetCalculator.load_user_input_to_dataframe(dataframe, user_input_dollars_to_budget) #loads userinput into dataframe
 dataframe = BudgetCalculator.calculate_all_budget_values(dataframe) #calculates all of the budget's values
 category_dollaramount_df = UserDisplay.select_columns(dataframe, ('category', 'dollar_amount'), hide_indexes_yn='y') # selects what to show
 print(category_dollaramount_df)
-
-
-
-
---
+++ commands/README.md
@@ -0,0 +1,15 @@
+
+
+User Interface Outline:
+
+*user opens program*
+*Big Ascii title appears, with commands of interest right below*
+*then user is prompted "what would you like to do? Type command here: "*
+
+commands folder:
+-has files that reperesent commands, containing the methods that make that command work within the class
+-each file has its own class to call to call the command
+-each class MUST have the same structure
+
+Main file:
+-files from commands file are imported depending on user input (variable import statement?)
